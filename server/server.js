23d8bdab4f3c7f67721a22f0c6cfca00676764ad
Tristan Barron <tristan_wilson_63@yahoo.com>
2023-01-04 11:30:18 -0600

    app.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const db = require('./config/connection');
 // const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
